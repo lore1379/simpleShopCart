7daa41181cc1e3287dded0d96a225cfa91feef94
Lorenzo <lorenzo.giannella@stud.unifi.it>
2021-12-16 16:23:31 +0100

    test checkoutProducts with several products in DB

--- com.mycompany.app.ShopCart/src/test/java/com/mycompany/shopcart/controller/ShopControllerTest.java
+++ com.mycompany.app.ShopCart/src/test/java/com/mycompany/shopcart/controller/ShopControllerTest.java
@@ -87,6 +87,20 @@ public class ShopControllerTest {
 		verify(productView).removeProductFromShop(product);
 	}
 	
-
+	@Test
+	public void testCheckoutProductsWhenSeveralProductAreInDatabase() {
+		Product product1 = new Product("1", "test1");
+		Product product2 = new Product("2", "test2");
+		List<Product> productsInCart = asList(product1, product2);
+		when(productRepository.findById("1")).thenReturn(product1);
+		when(productRepository.findById("2")).thenReturn(product2);
+		shopController.checkoutProducts(productsInCart);
+		verify(productRepository).delete("1");
+		verify(productRepository).delete("2");
+		verify(productView).removeProductFromCart(product1);
+		verify(productView).removeProductFromShop(product1);
+		verify(productView).removeProductFromCart(product2);
+		verify(productView).removeProductFromShop(product2);
+	}
 
 }
