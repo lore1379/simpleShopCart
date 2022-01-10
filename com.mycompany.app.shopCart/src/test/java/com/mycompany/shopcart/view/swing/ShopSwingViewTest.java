c15d5becc324ad98700f9570af5077fb0b6ba202
Lorenzo <lorenzo.giannella@stud.unifi.it>
2022-01-10 10:30:31 +0100

    created ShopSwingViewTest

---
+++ com.mycompany.app.shopCart/src/test/java/com/mycompany/shopcart/view/swing/ShopSwingViewTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.shopcart.view.swing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShopSwingViewTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
