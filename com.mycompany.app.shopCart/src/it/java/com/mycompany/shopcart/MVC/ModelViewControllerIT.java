8d63c008df8830723b213e31a57e0d5f7bd775fb
Lorenzo <lorenzo.giannella@stud.unifi.it>
2022-01-12 14:10:53 +0100

    MVC IT for Checkout

--- com.mycompany.app.shopCart/src/it/java/com/mycompany/shopcart/MVC/ModelViewControllerIT.java
+++ com.mycompany.app.shopCart/src/it/java/com/mycompany/shopcart/MVC/ModelViewControllerIT.java
@@ -2,12 +2,14 @@ package com.mycompany.shopcart.MVC;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.bson.Document;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.testcontainers.containers.MongoDBContainer;
 
@@ -16,6 +18,7 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mycompany.shopcart.controller.ShopController;
+import com.mycompany.shopcart.model.Product;
 import com.mycompany.shopcart.repository.mongo.ProductMongoRepository;
 import com.mycompany.shopcart.view.swing.ShopSwingView;
 
@@ -45,7 +48,6 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 		MongoDatabase database = mongoClient.getDatabase(SHOP_DB_NAME);
 		database.drop();
 		productCollection = database.getCollection(PRODUCT_COLLECTION_NAME);
-		
 		window = new FrameFixture(robot(), GuiActionRunner.execute(() -> {
 			shopSwingView = new ShopSwingView();
 			shopController = new ShopController(shopSwingView, productRepository);
@@ -58,6 +60,26 @@ public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
 	@Override
 	protected void onTearDown() {
 		mongoClient.close();
+	}
+	
+	@Test
+	public void testCheckout() {
+		Product product = new Product("99", "availableProduct");
+		addTestProductToDatabase(product.getId(), product.getName());
+		GuiActionRunner.execute(
+			() -> shopController.allProducts());
+		window.list("productList").selectItem(0);
+		window.button(JButtonMatcher.withText("Buy Selected")).click();
+		window.list("cartList").selectItem(0);
+		window.button(JButtonMatcher.withText("Checkout")).click();
+		assertThat(productRepository.findById("99")).isNull();
+	}
+	
+	private void addTestProductToDatabase(String id, String name) {
+		productCollection.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
 	}
 
 }
