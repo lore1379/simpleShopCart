d9bac816c56ad3f5346953412be65e2fc3756023
Lorenzo <lorenzo.giannella@stud.unifi.it>
2022-01-13 10:35:11 +0100

    created E2E test

---
+++ com.mycompany.app.shopCart/src/e2e/java/com/mycompany/shopcart/view/swing/ShopSwingAppE2E.java
@@ -0,0 +1,71 @@
+package com.mycompany.shopcart.view.swing;
+
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import javax.swing.JFrame;
+
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.mongodb.MongoClient;
+
+@RunWith(GUITestRunner.class)
+public class ShopSwingAppE2E extends AssertJSwingJUnitTestCase {
+	
+	@ClassRule
+	public static final MongoDBContainer mongo =
+		new MongoDBContainer("mongo:4.4.3");
+
+	private static final String DB_NAME = "test-db";
+	private static final String COLLECTION_NAME = "test-collection";
+	
+	private MongoClient mongoClient;
+
+	private FrameFixture window;
+
+	@Override
+	protected void onSetUp() {
+		String containerIpAddress = mongo.getContainerIpAddress();
+		Integer mappedPort = mongo.getFirstMappedPort();
+		mongoClient = new MongoClient(containerIpAddress, mappedPort);
+		mongoClient.getDatabase(DB_NAME).drop();
+		addTestProductToDatabase("1", "first product");
+		addTestProductToDatabase("2", "second product");
+		application("com.mycompany.shopcart.app.swing.ShopSwingApp")
+			.withArgs(
+				"--mongo-host=" + containerIpAddress,
+				"--mongo-port=" + mappedPort.toString(),
+				"--db-name=" + DB_NAME,
+				"--db-collection=" + COLLECTION_NAME
+			)
+			.start();
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+			@Override
+			protected boolean isMatching(JFrame frame) {
+				return "Shop View".equals(frame.getTitle()) && frame.isShowing();
+			}
+		}).using(robot());
+	}
+
+	@Override
+	protected void onTearDown() {
+		mongoClient.close();
+	}
+	
+	private void addTestProductToDatabase(String id, String name) {
+		mongoClient
+			.getDatabase(DB_NAME)
+			.getCollection(COLLECTION_NAME)
+			.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
+	}
+}
