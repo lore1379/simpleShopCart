79137134c377685e8bf0a602ebe6c4322b3050a2
Lorenzo <lorenzo.giannella@stud.unifi.it>
2022-01-17 10:41:41 +0100

    failing test for checkout with concurrent threads

---
+++ com.mycompany.app.shopCart/src/it/java/com/mycompany/shopcart/view/swing/ShopSwingViewRaceConditionIT.java
@@ -0,0 +1,94 @@
+package com.mycompany.shopcart.view.swing;
+
+import static org.awaitility.Awaitility.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.bson.Document;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mycompany.shopcart.controller.ShopController;
+import com.mycompany.shopcart.model.Product;
+import com.mycompany.shopcart.repository.mongo.ProductMongoRepository;
+import com.mycompany.shopcart.view.ProductView;
+
+@RunWith(GUITestRunner.class)
+public class ShopSwingViewRaceConditionIT extends AssertJSwingJUnitTestCase {
+		
+	@ClassRule
+	public static final MongoDBContainer mongo = new MongoDBContainer("mongo:4.4.3");
+	
+	@Mock
+	private ProductView productView;
+	
+	private MongoClient mongoClient;
+	private ProductMongoRepository productRepository;
+	private MongoCollection<Document> productCollection;
+	private AutoCloseable closeable;
+	
+	private static final String SHOP_DB_NAME = "shop";
+	private static final String PRODUCT_COLLECTION_NAME = "product";
+	
+	@Override
+	protected void onSetUp() {
+		closeable = MockitoAnnotations.openMocks(this);
+		mongoClient = new MongoClient(
+				new ServerAddress(
+						mongo.getContainerIpAddress(),
+						mongo.getFirstMappedPort()));
+		productRepository = new ProductMongoRepository(mongoClient, SHOP_DB_NAME, PRODUCT_COLLECTION_NAME);
+		MongoDatabase database = mongoClient.getDatabase(SHOP_DB_NAME);
+		database.drop();
+		productCollection = database.getCollection(PRODUCT_COLLECTION_NAME);
+	}
+	
+	@Override
+	protected void onTearDown() throws Exception{
+		mongoClient.close();
+		closeable.close();
+	}
+	
+	@Test @GUITest
+	public void testCheckoutWithConcurrentThreadsOnlyOneShouldCheckout() {
+		Product product = new Product("1", "test");
+		addTestProductToDatabase(product.getId(), product.getName());
+		List<Thread> threads = IntStream.range(0, 10)
+				.mapToObj(i -> new Thread(
+						() -> {
+							new ShopController(productView, productRepository).checkoutProduct(product);
+							})
+				)
+				.peek(t -> t.start())
+				.collect(Collectors.toList());
+		await().atMost(10, TimeUnit.SECONDS)
+			.until(() -> threads.stream().noneMatch(t -> t.isAlive()));
+		verify(productView, times(1)).checkoutProduct(product);
+	}
+	
+	private void addTestProductToDatabase(String id, String name) {
+		productCollection.insertOne(
+				new Document()
+					.append("id", id)
+					.append("name", name));
+	}
+	
+}
+	
+	
